78318ad6526f3c7f80f034f669e56d841ba3c221
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-04-10 19:51:09 -0300

    Posicionamento e movimentos do cavalo incluídos no método construtor

--- src/entities/PasseioDoCavalo.java
+++ src/entities/PasseioDoCavalo.java
@@ -13,6 +13,15 @@ public class PasseioDoCavalo {
 		this.vertical = new int[t];
 		this.horizontal = new int[t];
 		
+		horizontal[0] = 2; vertical[0] = -1;
+		horizontal[1] = 1; vertical[1] = -2;
+		horizontal[2] = -1; vertical[2] = -2;
+		horizontal[3] = -2; vertical[3] = -1;
+		horizontal[4] = -2; vertical[4] = 1;
+		horizontal[5] = -1; vertical[5] = 2;
+		horizontal[6] = 1; vertical[6] = 2;
+		horizontal[7] = 2; vertical[7] = 1;
+		
 	}
 
 }
