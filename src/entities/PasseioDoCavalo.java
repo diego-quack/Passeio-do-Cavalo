1980f568454177648d95707f8d22ca8126bbca32
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-04-10 20:31:32 -0300

    Método 'tenta' - seleciona movimentos e verifica se não estão furando os limites do tabuleiro

--- src/entities/PasseioDoCavalo.java
+++ src/entities/PasseioDoCavalo.java
@@ -1,34 +1,104 @@
 package entities;
 
 public class PasseioDoCavalo {
-	
+
 	private int t;
 	private int horizontal[];
 	private int vertical[];
 	private int tabuleiro[][];
-	
+
 	public PasseioDoCavalo(int t) {
 		this.t = t;
 		this.tabuleiro = new int[t][t];
 		this.vertical = new int[t];
 		this.horizontal = new int[t];
-		
-		horizontal[0] = 2; vertical[0] = -1;
-		horizontal[1] = 1; vertical[1] = -2;
-		horizontal[2] = -1; vertical[2] = -2;
-		horizontal[3] = -2; vertical[3] = -1;
-		horizontal[4] = -2; vertical[4] = 1;
-		horizontal[5] = -1; vertical[5] = 2;
-		horizontal[6] = 1; vertical[6] = 2;
-		horizontal[7] = 2; vertical[7] = 1;
-		
-		for(int i = 0; i < t; i++) {
-			for(int j = 0; j < t; j++) {
+
+		horizontal[0] = 2;
+		vertical[0] = -1;
+		horizontal[1] = 1;
+		vertical[1] = -2;
+		horizontal[2] = -1;
+		vertical[2] = -2;
+		horizontal[3] = -2;
+		vertical[3] = -1;
+		horizontal[4] = -2;
+		vertical[4] = 1;
+		horizontal[5] = -1;
+		vertical[5] = 2;
+		horizontal[6] = 1;
+		vertical[6] = 2;
+		horizontal[7] = 2;
+		vertical[7] = 1;
+
+		for (int i = 0; i < t; i++) {
+			for (int j = 0; j < t; j++) {
 				tabuleiro[i][j] = 0;
 				tabuleiro[0][0] = 1;
 			}
 		}
-		
 	}
-
+	
+	public boolean tenta(int i, int x, int y) {
+		int u;
+		int v;
+		int k;
+		boolean q;
+		
+		k = -1;
+		
+		do {
+			k = k + 1;
+			q = false;
+			u = x + horizontal[k];
+			v = y + vertical[k];
+			
+			if((u >= 0) && (u <= 7) && (v >= 0) && (v <= 7)) {
+				if(tabuleiro[u][v] == 0) {
+					tabuleiro[u][v] = i;
+					if(i < t * t) {
+						q = tenta(i + 1, u, v);
+						if(!q) {
+							tabuleiro[u][v] = 0;
+						}
+					}
+				}
+			}
+		}
+		while (!q && (k != 7));
+		return q;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
