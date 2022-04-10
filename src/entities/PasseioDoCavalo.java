dafc4dc18a6c8e4c58fe309ca21edc60c15c87bf
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-04-10 19:41:47 -0300

    Atributos e construtor - tabuleiro

--- src/entities/PasseioDoCavalo.java
+++ src/entities/PasseioDoCavalo.java
@@ -1,5 +1,18 @@
 package entities;
 
 public class PasseioDoCavalo {
+	
+	private int t;
+	private int horizontal[];
+	private int vertical[];
+	private int tabuleiro[][];
+	
+	public PasseioDoCavalo(int t) {
+		this.t = t;
+		this.tabuleiro = new int[t][t];
+		this.vertical = new int[t];
+		this.horizontal = new int[t];
+		
+	}
 
 }
